cb6e04bc8e6fbb90b6dbed9a7fe9d667fdd95a93
AlexanderGhosh <ghoshalexander@gmail.com>
2021-09-10 14:54:20 +0100

    should be all but determining the winner

--- Game.js
+++ Game.js
@@ -2,20 +2,29 @@ function Player(id, money){
   return {
     id: id,
     money: money,
-    cards: []
+    cards: [],
+    currentBet: 0
   };
 }
 
-let GameStates = {
+const GameStates = {
   New: 0,
   PreFlop: 1,
   Flop: 2,
   River: 3,
-  Turn: 4,
-  End: 5
+  End: 4,
 };
 
+const Actions = {
+  Fold: 0,
+  Call: 1,
+  Raise: 2,
+  Check: 3,
+
+}
+
 const MAX_PLAYERS = 4;
+const MAX_RAISES = 3;
 class Game{
   constructor(){
     this.players = [];
@@ -80,9 +89,30 @@ class Game{
       'Two Of Hearts',
     ]
     this.gameState = GameStates.New;
+    this.pot = 0;
+    this.blindPlayers = [];
+    this.minBets = [1, 2];
+    this.minBet = this.minBets[1];
+    this.currentPosition = 0;
+    this.hasFolded = []
+    this.hasBet = false;
+    this.counts = {
+      raise: 0,
+      check: 0,
+      call: 0
+    }
+    this.winner = 0;
+
+    this.reset = this.reset.bind();
   }
+
+  startGame(){
+    this.reset();
+    this.procesState();
+  }
+
   newPlayer(money){
-    if(this.players.length >= MAX_PLAYERS){
+    if(this.players.length >= MAX_PLAYERS || this.gameState != GameStates.New){
       return undefined;
     }
     let player = Player(this.players.length + 1, money);
@@ -98,8 +128,21 @@ class Game{
     }
   }
   reset(){
+    this.gameState = GameStates.New;
     this.players = [];
     this.selectedCards = [];
+    this.blindPlayers = [];
+    this.pot = 0;
+    this.minBet = this.minBets[1];
+    this.currentPosition = 1;
+    this.hasFolded = []
+    this.hasBet = false;
+    this.counts = {
+      raise: 0,
+      check: 0,
+      call: 0
+    }
+    this.winner = 0;
   }
   getRndCard(){
     if(this.selectedCards.length == 52){
@@ -124,37 +167,179 @@ class Game{
       player.cards.push(card);
     });
   }
-  advanceGame() {
+  dealCards(ammount) {
+    let cards = [];
+    for(let i = 0; i < ammount; i++){
+      let card = this.getRndCard();
+      cards.push(card);
+    }
+    return cards;
+  }
+  procesState() {
     switch (this.gameState) {
       case GameStates.New:
-        this.gameState = GameStates.PreFlop;
-        // deal the hands then take the blinds
+        // take the blinds then deal the hands then take bets
+
+        // determin the blind players
+        const numPlayers = players.length;
+        const id1 = Math.floor(Math.random() * numPlayers) + 1;
+        const id2 = id1 == 4 ? 1 : id1 + 1;
+        this.blindPlayers.push(id1);
+        this.blindPlayers.push(id2);
+        // take the blinds
+        this.makeBet(id1, this.minBets[0]);
+        this.makeBet(id2, this.minBets[1]);
+
+        // deal the hands the pre flop
+        this.dealHands();
+
+        // taking bets starting after the blind players
+        const id3 = id2 == 4 ? 1 : id2 + 1;
+        this.currentPosition = id3;
         break;
       case GameStates.PreFlop:
-        this.gameState = GameStates.Flop;
-        // take the bids then deal the flop
+        // deal the flop then take bets for the flop
+
+        // deals the flop
+        this.dealCards(3);
+        this.currentPosition = 1;
         break;
       case GameStates.Flop:
-        this.gameState = GameStates.River;
-        // take the bids then deal the river
+        // deal the river then take bets for the river
+
+        // deals the reiver
+        this.dealCards(1);
+        this.currentPosition = 1;
         break;
       case GameStates.River:
-        this.gameState = GameStates.Turn;
-        // take the bids then deal the turn
-        break;
-      case GameStates.Turn:
-        this.gameState = GameStates.End;
-        // take the bids
+        // deal the turn then take bets for the turn
+
+        // deals the turn
+        this.dealCards(1);
+        this.currentPosition = 1;
         break;
       case GameStates.End:
-        this.gameState = GameStates.New;
         // determin winner then reset the game
+        setInterval(this.reset 5000);
         break;
-      default:
-
     }
   }
-}
 
+  makeBet(id, bet){
+    let p = this.getPlayer(id);
+    if(p == undefined){
+      return 0;
+    }
+    else if(p.money < bet){
+      return 1;
+    }
+    else{
+      p.money -= bet;
+      this.pot += bet;
+      p.currentBet += bet;
+      return p;
+    }
+  }
+  getActions(id){
+    const p = this.getPlayer(id);
+    let actions = [Actions.Fold];
+    if(p.money > this.minBet && this.counts.raise < MAX_RAISES){
+      actions.push(Actions.Raise);
+    }
+    switch (this.gameState) {
+      case GameStates.New:
+        break;
+      case  GameStates.PreFlop:
+        if(this.blindPlayers[1] == id){
+          actions.push(Actions.Check);
+        }
+        else if(p.money >= this.minBet){
+          actions.push(Actions.Call);
+        }
+        break;
+      case  GameStates.Flop:
+      case  GameStates.River:
+      case  GameStates.Turn:
+        if(!this.hasBet){
+          actions.push(Actions.Check);
+        }
+        else if(p.money >= this.minBet){
+          actions.push(Actions.Call);
+        }
+        break;
+      case  GameStates.End:
+        break;
+    }
+  }
+  performAction(id, action, bet){
+    switch (action) {
+      case Actions.Fold:
+        this.hasFolded.push(id);
+        break;
+      case Actions.Call:
+        const p = this.getPlayer(id);
+        this.makeBet(id, this.minBet - p.currentBet);
+        this.hasBet = true;
+        this.counts.call += 1;
+        break;
+      case Action.Raise:
+        this.makeBet(id, bet);
+        this.minBet = bet;
+        this.hasBet = true;
+        this.counts.raise += 1;
+        break;
+      case Actions.Check:
+      this.counts.check += 1;
+        break;
+    }
+    const playerCount = this.players.length;
+    const inPlay = playerCount - this.hasFolded.length;
+    if(this.hasFolded.length == playerCount - 1){
+      // all but one have foled game over
+      let winner = 0;
+      for(let i = 0; i < playerCount; i++){
+        winner += i + 1;
+      }
+      for(let i = 0; i < this.hasFolded.length; i++){
+        winner -= this.hasFolded[i];
+      }
+      this.winner = winner;
+      return;
+    }
+
+    if(this.counts.check == inPlay){
+      // all players have checked this turn is over
+      this.advanceTurn();
+    }
+    if(this.counts.call == inPlay){
+      // all players have matched the bet this turn is over
+      this.advanceTurn();
+    }
+
+    do {
+      this.currentPosition += 1;
+      if(currentPosition == 5){
+        this.currentPosition = 1;
+      }
+    }
+    // if has folded then add again
+    while(this.hasFolded.includes(this.currentPosition));
+  }
+
+  advanceTurn(){
+    this.gameState += 1;
+    this.currentPosition = 0;
+    this.hasBet = false;
+    this.counts = {
+      raise: 0,
+      check: 0,
+      call: 0
+    }
+    this.players.forEach((item, i) => {
+      item.currentBet = 0;
+    });
+    this.procesState();
+  }
+}
 
 module.exports = {Game, Player, GameStates};
--- app.js
+++ app.js
@@ -17,7 +17,11 @@ app.get('/', function(req, res) {
 });
 
 app.get('/get', function(req, res) {
-  res.send(req.query);
+  res.json(game.players);
+});
+
+app.get('/pot', function(req, res) {
+  res.json(game.pot);
 });
 
 app.get('/cards/:id', function(req, res){
@@ -40,15 +44,31 @@ app.post('/create', function(req, res) {
   }
 });
 
+app.post('/bet', function(req, res){
+  const p = game.makeBet(req.body.id, req.body.bet);
+  if(p == 0){
+    res.status(404).send('Player not found');
+  }
+  else if(p == 1){
+    res.status(409).send('Unable to make bet');
+  }
+  else{
+    res.status(201).json(p);
+  }
+});
+
 app.put('/reset', function(req, res){
   game.reset();
   res.status(200).send('Game reset');
 });
 
-
 app.put('/start', function(req, res){
   game.dealHands();
   res.status(200).json(game.players);
+});
+
+app.get('/position', function(req, res){
+  res.status(200).json(game.currentPosition);
 });
 
 app.listen(port, () => {
